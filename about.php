8217f7f90641080421593f0b89b0c0835b454632
Navin Kumar Singh <113549400+NavinSingh1@users.noreply.github.com>
2023-05-02 23:14:02 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    <link rel="stylesheet" type="text/css" href="css/about.css">
+</head>
+
+<body style="background-color : #82E0AA;">
+
+<?php
+  include 'navbar.php';
+?>
+    
+    <div class="testimonials">
+        <div class="container">
+            <p class="text-center" style="font-size: 60px; margin-top: 20px;">Our Team Members</p>
+            <div class="row" style="margin-top: 100px;">
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="about-us-pp/navin.jpeg" class="user">
+                        <h3>Navin Kr. Singh <span>Frontend Developer</span></h3>
+                        <blockquote>As a frontend developer, I have developed the user interface for a banking management system that provides customers with a seamless banking experience. Through my expertise in HTML, CSS, and JavaScript, I have created a modern and intuitive design that allows customers to easily manage their accounts, view their transaction history, transfer funds, and more. I have also ensured that the system is responsive and accessible, making it easy to use on any device or platform. With my skills in frontend development, I have helped create a banking system that is not only functional but also visually appealing and user-friendly.</blockquote>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="about-us-pp/yadav.jpeg" class="user">
+                        <h3>Aniket Kr. Yadav <span>Database Developer</span></h3>
+                        <blockquote>As a database developer, I am responsible for designing, implementing, and maintaining the databases that are critical to the functioning of an organization. I work closely with other members of the development team to ensure that the database meets the needs of the organization and that it is scalable, reliable, and efficient.
+
+                        My job involves creating and modifying database structures, writing complex queries to extract and manipulate data, and optimizing database performance. I also work on data security and ensure that sensitive data is protected from unauthorized access. It is a challenging and rewarding role that plays a critical role in the success.</blockquote>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="about-us-pp/dev.jpeg" class="user">
+                        <h3>Mohini Deb <span>Website Designer</span></h3>
+                        <blockquote>As a website designer, my role in the banking management system is to create visually appealing and user-friendly interfaces that are easy to navigate. I work closely with the front-end developers to ensure that the design is implemented correctly and to make any necessary adjustments to the layout or graphics. I also collaborate with the database developers to ensure that the design elements are optimized for efficient data storage and retrieval. My goal is to create a cohesive and visually striking design that enhances the user experience and supports the overall goals of the banking management system. It is a challenging and rewarding role.</blockquote>
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="margin-top: 110px;">
+            <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="about-us-pp/sayan.jpeg" class="user">
+                        <h3>Sayan Basak <span>Database Designer</span></h3>
+                        <blockquote>As a database designer, my role is to design and implement a database structure that effectively stores and organizes data while ensuring data integrity, security, and scalability. I work closely with stakeholders to identify their data needs and requirements, and then create a data model that represents the relationships between various data entities. I also ensure that the database is optimized for performance and that it can handle large amounts of data. Additionally, I implement data backup and recovery procedures to ensure the safety and availability of the data. As a database designer, I must stay up-to-date with the latest database technologies and tools to ensure that our database systems are efficient and effective.</blockquote>
+                    </div>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="profile">
+                        <img src="about-us-pp/saptorshi.jpg" class="user">
+                        <h3>saptorshi Ch. <span>Backend Developer</span></h3>
+                        <blockquote>As a backend developer in a banking management system, my responsibilities include creating and maintaining the server-side logic that powers the application. This involves designing and implementing APIs that facilitate communication between the front-end and the database, ensuring data security and integrity, and optimizing database queries for improved performance. Additionally, I work closely with the database and server infrastructure teams to ensure that the application is scalable and highly available. My goal is to ensure that the banking management system operates smoothly and efficiently, providing a seamless experience for both customers and employees. I must stay up-to-date with the latest technologies.</blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/createuser.css">
+    <link rel="stylesheet" type="text/css" href="css/contact.css">
+</head>
+
+<body style="background-color : #82E0AA;">
+
+<?php
+  include 'navbar.php';
+?>
+
+<section class="services-container">
+        <h1 class="h-primary center">Contact-Us</h1>
+        <div id="services">
+            <div class="box">
+                <img src="./contact image/call.png" alt="">
+                <h2 class="h-secondary center">Call Us</h2>
+                <h4 style="text-align: center; font-size:24px;">+91 8282828282</h4>
+                <br><br>
+                <p class="center" style="color: grey;">Looking for assistance? Don't hesitate to give us a call! Our dedicated customer service team is available to answer your questions and provide support whenever you need it. Whether you have a question about our products or services, need help with an order, or simply want to provide feedback, we're here to help. </p>
+            </div>
+            <div class="box">
+                <img src="./contact image/email.jpg" alt="">
+                <h2 class="h-secondary center">Email Us</h2>
+                <h4 style="text-align: center; font-size:24px;">indianBank411@gmail.com</h4>
+                <br><br>
+                <p class="center" style="color: grey;">Have a question or concern that you'd like to discuss via email? No problem! Shoot us an email at support@company.com and one of our representatives will be in touch with you as soon as possible. We strive to provide the best customer service possible and look forward to hearing from you</p>
+            </div>
+            <div class="box">
+                <img src="./contact image/office.png" alt="">
+                <h2 class="h-secondary center">Visit Us</h2>
+                <h4 style="text-align: center; font-size:24px;">Indian Bank, EM-4, EM Block, Sector V, Bidhannagar, Kolkata, West Bengal 700091</h4>
+                <p class="center" style="color: grey;">Come visit us and experience our top-notch services for yourself! Our friendly staff is waiting to welcome you with open arms. We're conveniently located in the heart of the city, with easy access to public transportation and ample parking. Our modern and spacious facilities provide a comfortable and inviting environment for all our visitors. </p>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
